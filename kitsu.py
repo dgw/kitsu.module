88ddf566527ef9a677bf14c8143d25bd762cac3b
SleepingPanda <jovanphilip@gmail.com>
2018-05-11 18:35:51 -0400

    Update kitsu.py

--- kitsu.py
+++ kitsu.py
@@ -2,16 +2,16 @@ from sopel.module import commands, example
 import requests
 api = 'https://kitsu.io/api/edge/'
 aFilter = 'anime?page[limit]=5&filter[text]='
-@commands('kitsu', 'ka')
+@commands('kitsu')
 @example('.kitsu Clannad')
 def kitsu(bot, trigger):
 	query = trigger.group(2) or None
-	bot.say("[Kitsu] %s" % fetch_result(query))
+	bot.say("[Kitsu] " + fetch_result(query))
 def fetch_result(query):
 	if not query:
 		return "No search query provided."
 	try:
-		call = requests.get(url=api aFilter query, timeout=(10.0, 4.0))
+		call = requests.get(api + aFilter + query, timeout=(10.0, 4.0))
 	except requests.exceptions.ConnectTimeout:
 		return "Connection timed out."
 	except requests.exceptions.ConnectionError:
